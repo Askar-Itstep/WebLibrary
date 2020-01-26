e5f3bba72f3c0efbd2d99ba0096650315b141707
Аскар <@DESKTOP-Q5Q26T1>
2020-01-26 17:15:32 +0600

    first

---
+++ WebLibrary/Entities/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Обратите внимание, что authenticationType должен совпадать с типом, определенным в CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Здесь добавьте утверждения пользователя
+            return userIdentity;
+        }
+    }
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ WebLibrary/Entities/Model1.cs
@@ -0,0 +1,28 @@
+namespace DataLayer
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using DataLayer.Entities;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Authors> Authors { get; set; }
+        public virtual DbSet<Books> Books { get; set; }
+        public virtual DbSet<Users> Users { get; set; }
+        public virtual DbSet<Genres> Genres { get; set; }
+        public virtual DbSet<OrderBooks> OrderBooks { get; set; }
+        public virtual DbSet<Images> Images { get; set; }
+        public virtual DbSet<Statistic> Statistics { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
